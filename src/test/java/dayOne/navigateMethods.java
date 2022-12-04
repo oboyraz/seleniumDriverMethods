74a171e547c7b4a6e6de0ffa0311072734e5b928
oboyraz <oboyraz@sakarya.edu.tr>
2022-12-04 22:38:35 +0300

    navigateMethods

---
+++ src/test/java/dayOne/navigateMethods.java
@@ -0,0 +1,24 @@
+package dayOne;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class navigateMethods {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.com");
+        driver.navigate().to("https://www.trendyol.com");
+
+        driver.navigate().back(); // amazon
+        driver.navigate().forward(); // trendyol
+
+        driver.navigate().refresh(); // refreshPage
+
+        driver.close(); // close browser
+
+        // driver.quit(); // close all browser
+    }
+}
