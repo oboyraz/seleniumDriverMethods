22cacd60f9ad2218fa3ff33ee6e8fca23ae62533
oboyraz <oboyraz@sakarya.edu.tr>
2022-12-04 22:16:02 +0300

    driverMethods

---
+++ src/test/java/dayOne/driverMethods.java
@@ -0,0 +1,22 @@
+package dayOne;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class driverMethods {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.com/");
+        System.out.println("page title : " + driver.getTitle());
+        System.out.println("=".repeat(100));
+        System.out.println("page url: " + driver.getCurrentUrl());
+        System.out.println("=".repeat(100));
+        System.out.println(driver.getPageSource());
+        System.out.println("=".repeat(100));
+        System.out.println(driver.getWindowHandle()); // page unique hash code
+
+    }
+}
