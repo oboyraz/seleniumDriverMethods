3ffe3a6e59ad04175791aad3a16c280dd7529c05
omerboyraz <omer.boyraz@isuzu.com.tr>
2022-12-08 12:39:44 +0300

    assertionMethods

---
+++ src/test/java/seleniumWorks/assertionMethodTest.java
@@ -0,0 +1,54 @@
+package seleniumWorks;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class assertionMethodTest {
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void test01() {
+
+    driver.get("https:www.amazon.com");
+
+    WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+    searchBox.sendKeys("Nutella");
+    searchBox.submit();
+
+
+    WebElement textResultElement = driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+    String expectedResult = "Kutella";
+    String actualResult = textResultElement.getText();
+
+    Assert.assertTrue(actualResult.contains(expectedResult));
+
+        // Assert.assertFalse(condition)
+        // Assert.assertEqual(expected,actual)
+
+    }
+}
+
+
+
